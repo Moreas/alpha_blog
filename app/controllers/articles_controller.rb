75d5eb0c4bc9337ef6fd27d2bfa1714524d5f4e4
Florent Pignaud <florent.pignaud@gmail.com>
2017-01-17 15:09:38 -0500

    CRUD actions and views completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 	def new
 		@article=Article.new
@@ -14,7 +15,38 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
+	def index
+		@articles=Article.all
+	end
+
+
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@article.destroy
+		flash[:notice] = "Article was successfully deleted"
+		redirect_to articles_path
+	end
+
+
 	private
+	def set_article
+		@article=Article.find(params[:id])
+	end
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
